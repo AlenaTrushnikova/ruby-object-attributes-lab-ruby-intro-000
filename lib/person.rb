308165575e795fc3694cf2a9902168fb5119c71b
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-14 04:25:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,18 @@
 class Person
-  def
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
   
   
   

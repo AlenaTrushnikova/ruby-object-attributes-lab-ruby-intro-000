7e8d6f47b84682ad4d5d576ea86803c445a587a8
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-14 04:25:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,6 @@ class Dog
   def name=(name)
     @name = name
   end
-
   def name
     @name
   end
@@ -10,7 +9,6 @@ class Dog
   def breed=(breed)
     @breed = breed
   end
-
   def breed
     @breed
   end
